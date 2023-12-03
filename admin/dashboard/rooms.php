9fa6196cd285cca94421a254a2b5b4cddc82cb9f
Aeron Umali <aeronumali345@gmail.com>
2023-12-03 21:12:26 +0800

    Fixed rooms table

--- admin/dashboard/rooms.php
+++ admin/dashboard/rooms.php
@@ -22,11 +22,11 @@ $result = $crud->read($sql);
         <div class="row">
             <div class="col-sm-8 col-sm-offset-2">
                 <?php
-				if(isset($_SESSION['message'])) {
-					echo '<div class="alert alert-info text-center">'.$_SESSION['message'].'</div>';
-					unset($_SESSION['message']);
-				}
-				?>
+                if(isset($_SESSION['message'])) {
+                    echo '<div class="alert alert-info text-center">'.$_SESSION['message'].'</div>';
+                    unset($_SESSION['message']);
+                }
+                ?>
 
                 <a href="#add" data-toggle="modal" class="btn btn-primary">Add New</a><br><br>
                 <table class="table table-bordered table-striped">
@@ -36,22 +36,14 @@ $result = $crud->read($sql);
                             <th>Rent</th>
                             <th>Status</th>
                             <th>Action</th>
-
                         </tr>
                     </thead>
                     <tbody>
                         <?php foreach($result as $row): ?>
                         <tr>
-                            <td>
-                                <?php echo $row['id']; ?>
-                            </td>
-                            <td>
-                                <?php echo $row['rent']; ?>
-                            </td>
-                            <td>
-                                <?php echo $row['status']; ?>
-                            </td>
-
+                            <td><?php echo $row['id']; ?></td>
+                            <td><?php echo $row['rent']; ?></td>
+                            <td><?php echo $row['status']; ?></td>
                             <td>
                                 <a href="#edit<?php echo $row['id']; ?>" data-toggle="modal"
                                     class="btn btn-success">Edit</a> |
@@ -60,31 +52,28 @@ $result = $crud->read($sql);
                             </td>
 
                             <?php
-								// Edit modal
-								$modalId = 'edit'.$row['id'];
-								$modalTitle = 'Edit Rooms';
-								$formAction = '../../actions/roomsAction.php?id='.$row['id'];
-								$submitBtnName = 'edit';
-								$submitBtnText = 'Save';
-								$formFields = [
-									['label' => 'ID', 'name' => 'id', 'type' => 'text', 'value' => $row['id']],
-									['label' => 'Rent', 'name' => 'rent', 'type' => 'text', 'value' => $row['rent']],
-									['label' => 'Status', 'name' => 'status', 'type' => 'text', 'value' => $row['status']]
-							
-								];
-								include('../../components/modal.php');
+                                $modalId = 'edit'.$row['id'];
+                                $modalTitle = 'Edit Rooms';
+                                $formAction = '../../actions/roomsAction.php?id='.$row['id'];
+                                $submitBtnName = 'edit';
+                                $submitBtnText = 'Save';
+                                $formFields = [
+                                    ['label' => 'ID', 'name' => 'id', 'type' => 'text', 'value' => $row['id']],
+                                    ['label' => 'Rent', 'name' => 'rent', 'type' => 'text', 'value' => $row['rent']],
+                                    ['label' => 'Status', 'name' => 'status', 'type' => 'select', 'options' => getStatusOptions($row['status'])],
+                                ];
+                                include('../../components/modal.php');
 
-								// Delete modal
-								$modalId = 'delete'.$row['id'];
-								$modalTitle = 'Delete Admin';
-								$formAction = '../../actions/roomsAction.php?id='.$row['id'];
-								$submitBtnName = 'delete';
-								$submitBtnText = 'Delete';
-								$formFields = [
-									['label' => 'ID', 'name' => 'id', 'type' => 'text', 'value' => $row['id']]
-								];
-								include('../../components/modal.php');
-								?>
+                                $modalId = 'delete'.$row['id'];
+                                $modalTitle = 'Delete Admin';
+                                $formAction = '../../actions/roomsAction.php?id='.$row['id'];
+                                $submitBtnName = 'delete';
+                                $submitBtnText = 'Delete';
+                                $formFields = [
+                                    ['label' => 'ID', 'name' => 'id', 'type' => 'text', 'value' => $row['id']],
+                                ];
+                                include('../../components/modal.php');
+                                ?>
                         </tr>
                         <?php endforeach; ?>
                     </tbody>
@@ -94,21 +83,28 @@ $result = $crud->read($sql);
     </div>
 
     <?php
-	// Add modal
-	$modalId = 'add';
-	$modalTitle = 'Add Admin';
-	$formAction = '../../actions/roomsAction.php';
-	$submitBtnName = 'add';
-	$submitBtnText = 'Save';
-	$formFields = [
-	['label' => 'Rent', 'name' => 'rent', 'type' => 'text'],
-	['label' => 'Status', 'name' => 'status', 'type' => 'text'],
-	];
-	include('../../components/modal.php');
-	?>
+    $modalId = 'add';
+    $modalTitle = 'Add Admin';
+    $formAction = '../../actions/roomsAction.php';
+    $submitBtnName = 'add';
+    $submitBtnText = 'Save';
+    $formFields = [
+        ['label' => 'Rent', 'name' => 'rent', 'type' => 'text'],
+        ['label' => 'Status', 'name' => 'status', 'type' => 'select', 'options' => getStatusOptions()],
+    ];
+    include('../../components/modal.php');
+    ?>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 
 </html>
+
+<?php
+function getStatusOptions($selectedStatus = null) {
+    $options = '<option value="Available" ' . ($selectedStatus == 'Available' ? 'selected' : '') . '>Available</option>';
+    $options .= '<option value="Occupied" ' . ($selectedStatus == 'Occupied' ? 'selected' : '') . '>Occupied</option>';
+    return $options;
+}
+?>
