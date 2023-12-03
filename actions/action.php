158082597720dac923e1c2e14a984a4ba3275453
Leo Mikko Uson <usonleomikko@gmail.com>
2023-12-03 18:46:34 +0800

    Add CRUD functionality for Admin model

---
+++ actions/action.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+include_once('../conn/Crud.php');
+
+$crud = new Crud();
+
+if(isset($_POST['add'])) {
+    $first_name = $crud->escape_string($_POST['first_name']);
+    $last_name = $crud->escape_string($_POST['last_name']);
+    $email = $crud->escape_string($_POST['email']);
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    // Insert data into the database
+    $sql = "INSERT INTO Admin (first_name, last_name, email, password) VALUES ('$first_name', '$last_name', '$email', '$password')";
+
+    if($crud->execute($sql)) {
+        $_SESSION['message'] = 'Admin added successfully';
+    } else {
+        $_SESSION['message'] = 'Cannot add admin';
+    }
+
+    header('location: ../admin/dashboard');
+} elseif(isset($_POST['edit'])) {
+    $id = $crud->escape_string($_POST['id']);
+    $first_name = $crud->escape_string($_POST['first_name']);
+    $last_name = $crud->escape_string($_POST['last_name']);
+    $email = $crud->escape_string($_POST['email']);
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    // Update data in the database
+    $sql = "UPDATE Admin SET first_name = '$first_name', last_name = '$last_name', email = '$email', password = '$password' WHERE id = '$id'";
+
+    if($crud->execute($sql)) {
+        $_SESSION['message'] = 'Admin updated successfully';
+    } else {
+        $_SESSION['message'] = 'Cannot update admin';
+    }
+
+    header('location: ../admin/dashboard');
+} elseif(isset($_POST['delete'])) {
+    $id = $crud->escape_string($_POST['id']);
+
+    // Delete data from the database
+    $sql = "DELETE FROM Admin WHERE id = '$id'";
+
+    if($crud->execute($sql)) {
+        $_SESSION['message'] = 'Admin deleted successfully';
+    } else {
+        $_SESSION['message'] = 'Cannot delete admin';
+    }
+
+    header('location: ../admin/dashboard');
+} else {
+    $_SESSION['message'] = 'Invalid action';
+    header('location: ');
+}
+?>
