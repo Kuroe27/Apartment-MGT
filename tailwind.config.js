e08368c3fc8747dc15e77b01d18076a8c1fbec4b
Leo Mikko Uson <usonleomikko@gmail.com>
2023-12-06 12:41:26 +0800

    Add Tailwind CSS and configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./admin/**/*.{html,js,php}",
+    "./tenants/**/*.{html,js,php}",
+    "./components/**/*.{html,js,php}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        pallete: {
+          100: "#EEF1FF",
+          200: "#D2DAFF",
+          300: "#AAC4FF",
+          400: "#B1B2FF",
+          500: "#98A8F8",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
