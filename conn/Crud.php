4145bbcab6adfabeb7aad71086e4e7165b61f271
Leo Mikko Uson <usonleomikko@gmail.com>
2023-12-06 12:42:08 +0800

    Refactor Crud class constructor and methods

--- conn/Crud.php
+++ conn/Crud.php
@@ -1,43 +1,42 @@
 <?php
 include_once('DbConnection.php');
- 
-class Crud extends DbConnection
-{
-    public function __construct(){
+
+class Crud extends DbConnection {
+    public function __construct() {
 
         parent::__construct();
     }
-    
-    public function read($sql){
+
+    public function read($sql) {
 
         $query = $this->connection->query($sql);
-        
-        if ($query == false) {
+
+        if($query == false) {
             return false;
-        } 
-        
+        }
+
         $rows = array();
-        
-        while ($row = $query->fetch_array()) {
+
+        while($row = $query->fetch_array()) {
             $rows[] = $row;
         }
-        
+
         return $rows;
     }
-        
-    public function execute($sql){
+
+    public function execute($sql) {
 
         $query = $this->connection->query($sql);
-        
-        if ($query == false) {
+
+        if($query == false) {
             return false;
         } else {
             return true;
-        }        
+        }
     }
-    
-    public function escape_string($value){
-        
+
+    public function escape_string($value) {
+
         return $this->connection->real_escape_string($value);
     }
 }
