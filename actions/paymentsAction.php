a5cb67d9dc3a63ba5c6e80d91484ab29dc583640
Leo Mikko Uson <usonleomikko@gmail.com>
2023-12-08 15:18:22 +0800

    Fix tenant ID retrieval in paymentsAction.php

--- actions/paymentsAction.php
+++ actions/paymentsAction.php
@@ -5,21 +5,31 @@ include_once('../conn/Crud.php');
 $crud = new Crud();
 
 if(isset($_POST['add'])) {
-    $tenant_id = $crud->escape_string($_POST['tenant_id']);
     $invoice_id = $crud->escape_string($_POST['invoice_id']);
     $amount_paid = $crud->escape_string($_POST['amount_paid']);
     $payment_date = $crud->escape_string($_POST['payment_date']);
 
-    $sql = "INSERT INTO `Payments`(`tenant_id`, `invoice_id`, `amount_paid`, `payment_date`)
-            VALUES ('$tenant_id', '$invoice_id', '$amount_paid', '$payment_date')";
+    // Fetch the tenant ID based on the selected invoice ID
+    $sqlFetchTenantId = "SELECT tenant_id FROM Invoices WHERE id = '$invoice_id'";
+    $result = $crud->read($sqlFetchTenantId);
 
-    if($crud->execute($sql)) {
-        $_SESSION['message'] = 'Payment added successfully';
+    if($result && count($result) > 0) {
+        $tenant_id = $result[0]['tenant_id'];
+
+        $sql = "INSERT INTO `Payments`(`tenant_id`, `invoice_id`, `amount_paid`, `payment_date`)
+                VALUES ('$tenant_id', '$invoice_id', '$amount_paid', '$payment_date')";
+
+        if($crud->execute($sql)) {
+            $_SESSION['message'] = 'Payment added successfully';
+        } else {
+            $_SESSION['message'] = 'Cannot add payment';
+        }
     } else {
-        $_SESSION['message'] = 'Cannot add payment';
+        $_SESSION['message'] = 'Unable to fetch tenant information for the selected invoice';
     }
 
     header('location: ../admin/dashboard/payments.php');
+
 } elseif(isset($_POST['edit'])) {
     $id = $crud->escape_string($_POST['id']);
     $tenant_id = $crud->escape_string($_POST['tenant_id']);
