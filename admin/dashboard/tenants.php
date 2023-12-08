e0f57d92354fbd76c19e443144eb77b372cbb2a5
Leo Mikko Uson <usonleomikko@gmail.com>
2023-12-08 15:19:16 +0800

    updated tenants

--- admin/dashboard/tenants.php
+++ admin/dashboard/tenants.php
@@ -3,153 +3,184 @@ session_start();
 include_once('../../conn/Crud.php');
 
 $crud = new Crud();
-$sql = "SELECT t.*, r.rent, r.status AS room_status FROM Tenants t LEFT JOIN rooms r ON t.room_id = r.id";
-$result = $crud->read($sql);
+$sql = "SELECT * FROM Tenants";
+$tenants = $crud->read($sql);
+$roomOptions = [];
+
+$sqlRooms = "SELECT id, status FROM Rooms WHERE status = 'Available'";
+$rooms = $crud->read($sqlRooms);
+
+foreach($rooms as $room) {
+    $roomOptions[$room['id']] = $room['id'].' ('.$room['status'].')';
+}
+
+
+
+$sqlRooms = "SELECT id, status FROM Rooms";
+$rooms = $crud->read($sqlRooms);
+
+foreach($rooms as $room) {
+    $roomOption[$room['id']] = $room['id'].' ('.$room['status'].')';
+}
+
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <title>PHP OOP </title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="path-to-your-tailwind.css">
+    <title>Admin Page</title>
+    <link href="../../styles/output.css" rel="stylesheet" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 
-<body>
-    <div class="container">
-        <h1 class="page-header text-center">PHP CRUD using Object Oriented Programming</h1>
-        <div class="row">
-            <div class="col-sm-8 col-sm-offset-2">
-                <?php
-                if(isset($_SESSION['message'])) {
-                    echo '<div class="alert alert-info text-center">'.$_SESSION['message'].'</div>';
-                    unset($_SESSION['message']);
-                }
-                ?>
+<body class="h-100vh flex">
+    <?php include_once('../../components/sidebar.php') ?>
 
-                <a href="#add" data-toggle="modal" class="btn btn-primary">Add New</a><br><br>
-                <table class="table table-bordered table-striped">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Email</th>
-                            <th>Move In Date</th>
-                            <th>Balance</th>
-                            <th>Room ID</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach($result as $row): ?>
-                            <tr>
-                                <td>
-                                    <?php echo $row['id']; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row['first_name']; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row['last_name']; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row['email']; ?>
-                                </td>
-
-                                <td>
-                                    <?php echo $row['move_in_date']; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row['balance']; ?>
-                                </td>
-                                <td>
-                                    <?php echo $row['room_id']; ?>
-                                </td>
-
-
-
-                                <td>
-                                    <a href="#edit<?php echo $row['id']; ?>" data-toggle="modal"
-                                        class="btn btn-success">Edit</a> |
-                                    <a href="#delete<?php echo $row['id']; ?>" data-toggle="modal"
-                                        class="btn btn-danger">Delete</a>
-                                </td>
-
-                                <?php
-                                // Edit modal
-                                $modalId = 'edit'.$row['id'];
-                                $modalTitle = 'Edit Tenant';
-                                $formAction = '../../actions/tenantsAction.php?id='.$row['id'];
-                                $submitBtnName = 'edit';
-                                $submitBtnText = 'Save';
-                                $formFields = [
-                                    ['label' => 'ID', 'name' => 'id', 'type' => 'text', 'value' => $row['id']],
-                                    ['label' => 'First Name', 'name' => 'first_name', 'type' => 'text', 'value' => $row['first_name']],
-                                    ['label' => 'Last Name', 'name' => 'last_name', 'type' => 'text', 'value' => $row['last_name']],
-                                    ['label' => 'Email', 'name' => 'email', 'type' => 'text', 'value' => $row['email']],
-                                    ['label' => 'Move In Date', 'name' => 'move_in_date', 'type' => 'date', 'value' => $row['move_in_date']],
-                                    ['label' => 'Balance', 'name' => 'balance', 'type' => 'text', 'value' => $row['balance']],
-                                    ['label' => 'Room ID', 'name' => 'room_id', 'type' => 'select', 'options' => getRoomOptions($crud, $row['room_id'])],
-                                    ['label' => 'Password', 'name' => 'password', 'type' => 'text', 'value' => $row['password']]
-                                ];
-                                include('../../components/modal.php');
-
-                                // Delete modal
-                                $modalId = 'delete'.$row['id'];
-                                $modalTitle = 'Delete Tenant';
-                                $formAction = '../../actions/tenantsAction.php?id='.$row['id'];
-                                $submitBtnName = 'delete';
-                                $submitBtnText = 'Delete';
-                                $formFields = [
-                                    ['label' => 'ID', 'name' => 'id', 'type' => 'text', 'value' => $row['id']]
-                                ];
-                                include('../../components/modal.php');
-                                ?>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+    <main class="ml-0 sm:ml-60 w-full">
+        <nav class="container px-4 h-16 w-full mt-4 z-10 flex items-center text-center justify-center mx-auto">
+            <div class="flex items-center">
+                <button data-drawer-target="sidebar" data-drawer-toggle="sidebar" aria-controls="sidebar" type="button"
+                    onclick="toggleSidebar()"
+                    class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2  focus:ring-gray-600">
+                    <span class="material-symbols-outlined" onclick="toggleSidebar()" data-drawer-target="sidebar"
+                        data-drawer-toggle="sidebar" aria-controls="sidebar" type="button">
+                        menu
+                    </span>
+                </button>
+                <h1 class="text-2xl font-semibold">Tenants</h1>
             </div>
-        </div>
-    </div>
+            <div class="flex items-center ml-auto">
+            </div>
+        </nav>
+
+        <section class="bg min-h-[calc(100vh-80px)] flex flex-column ">
+            <div class="container mx-auto p-4">
+                <div class="bg-white  p-4 shadow rounded-md ">
+                    <?php
+                    if(isset($_SESSION['message'])) {
+                        echo '<div class="bg-blue-200 text-blue-800 p-4 mb-4">'.$_SESSION['message'].'</div>';
+                        unset($_SESSION['message']);
+                    }
+                    ?>
+                    <a href="#add" id="openAddModalBtn" class="bg-pallete-400 text-white py-2 px-4 rounded"
+                        onclick="openAddModal()">Add New</a><br><br>
+                    <div class="relative overflow-x-auto ">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase  ">
+                                <tr>
+                                    <th class="px-6 py-3">ID</th>
+                                    <th class="px-6 py-3">First Name</th>
+                                    <th class="px-6 py-3">Last Name</th>
+                                    <th class="px-6 py-3">Email</th>
+                                    <th class="px-6 py-3">Move In Date</th>
+                                    <th class="px-6 py-3">Balance</th>
+                                    <th class="px-6 py-3">Room ID</th>
+                                    <th class="px-6 py-3">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach($tenants as $tenant): ?>
+                                    <tr class="bg-white border-t">
+                                        <td class="w-4 p-4 pl-6">
+                                            <?php echo $tenant['id']; ?>
+                                        </td>
+                                        <td class="w-4 p-4 pl-6">
+                                            <?php echo $tenant['first_name']; ?>
+                                        </td>
+                                        <td class="w-4 p-4 pl-6">
+                                            <?php echo $tenant['last_name']; ?>
+                                        </td>
+                                        <td class="w-4 p-4 pl-6">
+                                            <?php echo $tenant['email']; ?>
+                                        </td>
+                                        <td class="w-4 p-4 pl-6">
+                                            <?php echo $tenant['move_in_date']; ?>
+                                        </td>
+                                        <td class="w-4 p-4 pl-6">
+                                            <?php echo $tenant['balance']; ?>
+                                        </td>
+                                        <td class="w-4 p-4 pl-6">
+                                            <?php echo $tenant['room_id']; ?>
+                                        </td>
+                                        <td class="w-4 p-4 pl-6 ">
+                                            <div class="flex">
+                                                <img src="../../images/edit.png" alt="" class="w-4 mr-2 cursor-pointer"
+                                                    onclick="openEditModal('<?php echo $tenant['id']; ?>')">
+                                                <img src="../../images/delete.png" alt="" class="w-4 mr-2 cursor-pointer"
+                                                    onclick="openDeleteModal('<?php echo $tenant['id']; ?>')">
+                                            </div>
+                                        </td>
+                                    </tr>
+
+                                    <?php
+                                    // Edit modal
+                                    $modalId = 'edit'.$tenant['id'];
+                                    $modalTitle = 'Edit Tenant';
+                                    $formAction = '../../actions/tenantsAction.php?id='.$tenant['id'];
+                                    $submitBtnName = 'edit';
+                                    $submitBtnText = 'Save';
+
+
+                                    $selectedRoomId = $tenant['room_id'];
+
+                                    $formFields = [
+                                        ['label' => 'ID', 'name' => 'id', 'type' => 'text', 'value' => $tenant['id']],
+                                        ['label' => 'First Name', 'name' => 'first_name', 'type' => 'text', 'value' => $tenant['first_name']],
+                                        ['label' => 'Last Name', 'name' => 'last_name', 'type' => 'text', 'value' => $tenant['last_name']],
+                                        ['label' => 'Email', 'name' => 'email', 'type' => 'email', 'value' => $tenant['email']],
+                                        ['label' => 'Move In Date', 'name' => 'move_in_date', 'type' => 'date', 'value' => $tenant['move_in_date']],
+                                        ['label' => 'Balance', 'name' => 'balance', 'type' => 'number', 'value' => $tenant['balance']],
+                                        ['label' => 'Room ID', 'name' => 'room_id', 'type' => 'select', 'selected' => $selectedRoomId, 'options' => $roomOption],
+                                    ];
+
+                                    include('../../components/modal.php');
+
+                                    // Delete modal
+                                    $modalId = 'delete'.$tenant['id'];
+                                    $modalTitle = 'Delete Tenant';
+                                    $formAction = '../../actions/tenantsAction.php?id='.$tenant['id'];
+                                    $submitBtnName = 'delete';
+                                    $submitBtnText = 'Delete';
+                                    $formFields = [
+                                        ['label' => 'ID', 'name' => 'id', 'type' => 'text', 'value' => $tenant['id']],
+                                    ];
+
+                                    include('../../components/modal.php');
+                                    ?>
+                                <?php endforeach; ?>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+        </section>
+    </main>
 
     <?php
-    // Add modal
     $modalId = 'add';
     $modalTitle = 'Add Tenant';
     $formAction = '../../actions/tenantsAction.php';
     $submitBtnName = 'add';
     $submitBtnText = 'Save';
+
     $formFields = [
         ['label' => 'First Name', 'name' => 'first_name', 'type' => 'text'],
         ['label' => 'Last Name', 'name' => 'last_name', 'type' => 'text'],
-        ['label' => 'Email', 'name' => 'email', 'type' => 'text'],
+        ['label' => 'Email', 'name' => 'email', 'type' => 'email'],
         ['label' => 'Move In Date', 'name' => 'move_in_date', 'type' => 'date'],
-        ['label' => 'Room ID', 'name' => 'room_id', 'type' => 'select', 'options' => getRoomOptions($crud)],
-        ['label' => 'Password', 'name' => 'password', 'type' => 'text']
+        ['label' => 'Balance', 'name' => 'balance', 'type' => 'number'],
+        ['label' => 'Room ID', 'name' => 'room_id', 'type' => 'select', 'options' => $roomOptions],
     ];
+
     include('../../components/modal.php');
-
-    // Function to get room options
-    function getRoomOptions($crud, $selectedRoomId = null) {
-        $sql = "SELECT id FROM rooms";
-        $rooms = $crud->read($sql);
-
-        $options = '<option value="">Select Room</option>';
-        foreach($rooms as $room) {
-            $selected = ($selectedRoomId == $room['id']) ? 'selected' : '';
-            $options .= '<option value="'.$room['id'].'" '.$selected.'>'.$room['id'].'</option>';
-        }
-
-        return $options;
-    }
-
     ?>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="../../scripts/index.js"></script>
 </body>
 
 </html>
